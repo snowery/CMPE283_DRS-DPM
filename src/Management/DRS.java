ba353591d067393b427d74962c9a52aeb12aec1e
Yang Song <ysong819@gmail.com>
2014-05-04 14:51:10 -0700

    DRS now migrate the lowest usage VM

--- src/Management/DRS.java
+++ src/Management/DRS.java
@@ -1,7 +1,6 @@
 package Management;
 
 
-import java.util.ArrayList;
 import java.util.List;
 
 import Instances.VHost;
@@ -9,52 +8,70 @@ import Instances.VM;
 
 import com.vmware.vim25.mo.ServiceInstance;
 
-public class DRS extends Manager{
+public class DRS extends Manager {
 	public DRS(ServiceInstance si) throws Exception {
-		super(si);
+		super(si);																//contractor 
 	}
 	
 	public void start() throws Exception {
 		System.out.println("List all virtual hosts: ");
 		
 		for(int i=0; i<vHosts.size(); i++) {
-			VHost currentHost = vHosts.get(i);
-			System.out.println("\n==============================================");
-			System.out.println("Current Host: " + currentHost.getName());
-			System.out.println("Current host usage: " + currentHost.cpuUsageMhz() + "Mhz");
+			VHost currentHost = vHosts.get(i); 									//get the current host
+			System.out.println("\n========================================================");
+			System.out.println("Current Host: " + 
+									currentHost.getName()); 					//print current host name
+			System.out.println("Current host usage: " + 
+									currentHost.cpuUsageMhz() + "Mhz");			//print current host cpu usage
 			//List<VHost> restHosts = new ArrayList<VHost>();
 			//restHosts = tempHost(vHosts.get(i));
-			VHost lowestUsageHost = getLowestUsageHost(vHosts);
-			System.out.println("Lowest host: " + lowestUsageHost.getName());
-			System.out.println("Lowest host usage: " + lowestUsageHost.cpuUsageMhz() + "Mhz");
+			VHost lowestUsageHost = getLowestUsageHost(vHosts);					//get lowest usage host in poll
+			System.out.println("Lowest host: " + 
+									lowestUsageHost.getName());
+			System.out.println("Lowest host usage: " + 
+									lowestUsageHost.cpuUsageMhz() + "Mhz");
 			System.out.println();
 			
-			if(isOverload(currentHost)) {
+			if(isOverload(currentHost)) {										//check current host is overload or not
 				System.out.println("Current host usage is high!");
-				List<VM> vms = currentHost.getVMs();
-				VM lowestUsageVm = getLowestUsageVm(vms);
-				
-				for(VM vm : vms) {
-					System.out.println("Virtual machine: " + vm.getName());
-					System.out.println("Virtual machine usage: " + vm.cpuUsageMhz() + "Mhz");
+				List<VM> vms = currentHost.getVMs();							//get vm list under current host
+				VM lowestUsageVm = getLowestUsageVm(vms);						//get lowest usage vm
+				//System.out.println(lowestUsageVm);
+				//for(VM vm : vms) {											//loop all vms under this host
+					System.out.println("Lowest usage virtual machine: " + 
+											lowestUsageVm.getName());
+					System.out.println("Lowest usage virtual machine usage: " + 
+											lowestUsageVm.cpuUsageMhz(1) + "Mhz");
 					
-					if(currentHost==lowestUsageHost) {
-						System.out.println("Current host already the lowest usage host, no need to do migrate\n");
-					} else {
+					if(currentHost==lowestUsageHost) {							//check if the current host already the 
+						System.out.println("Current host already the "			//lowest usage host, do nothing
+								+ "lowest usage host, no need to do migrate\n");
+					} else {													//else doing simulation migrate
 						
-						if(!isOverloadAfterMigrate(lowestUsageHost, vm.cpuUsageMhz())) {
-							//&& isOverloadAfterMigrate(lowestUsageHost, (Math.abs(vm.cpuUsageMhz()) * -1))) {
-						System.out.println("Simulation migrate pass");
-						System.out.println("Virtual machine: " + vm.getName() + " start migrate to new host: " + lowestUsageHost.getName());
-						vm.migrate(lowestUsageHost);
+						if(!isOverloadAfterMigrate(lowestUsageHost, 
+													lowestUsageVm.cpuUsageMhz())) {			//if the destination host's usage
+						System.out.println("Simulation migrate pass");						//plus the vm's usage still
+						System.out.println("Virtual machine: " + 							//under control, doing migrate
+													lowestUsageVm.getName() + " start migrate to new host: " + 
+													lowestUsageHost.getName());
+						lowestUsageVm.migrate(lowestUsageHost);
+						} 
+						else if(isOverloadAfterMigrate(lowestUsageHost, 
+													lowestUsageVm.cpuUsageMhz())			//if the destination host's usage
+							&& isOverloadAfterMigrate(lowestUsageHost, 						//plus the vm's usage is high, and
+													(Math.abs(lowestUsageVm.cpuUsageMhz()) * -1))) { 	//vm's current host's usage after
+							System.out.println("Simulation migrate not pass");				//migrate still high, power on a 
+							System.out.println("Current distination host usage also high");	//new host
+							System.out.println("Power on new host, migrate vm to new host");
 						} else {
-							System.out.println("Simulation migrate not pass");
-							System.out.println("Virtual machine : " + vm.getName() + " : no need to do migrate\n");
+							System.out.println("Simulation migrate not pass");				//for other situation, do not 
+							System.out.println("Virtual machine : " + 						//doing any migrate
+									lowestUsageVm.getName() + " : no need to do migrate\n");
 						}
 					}
-				}
+				//}
 			} else {
-				System.out.println("System fine");
+				System.out.println("System fine");								//if host is not overload, print system fine
 			}
 		}
 	}
